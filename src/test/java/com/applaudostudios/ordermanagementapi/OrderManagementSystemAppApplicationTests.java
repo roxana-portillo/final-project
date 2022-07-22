c2dd781b569bd0c04fd7c93c37092aa758d2cea4
Roxana Portillo <roxanaportillo221b@gmail.com>
2022-07-21 23:32:17 -0600

    Initial project set up

---
+++ src/test/java/com/applaudostudios/ordermanagementapi/OrderManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applaudostudios.ordermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
